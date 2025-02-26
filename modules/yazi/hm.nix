4e8b2b8836dd50b9231dc606e23b0938665c316c
Edgar B <39066502+Guekka@users.noreply.github.com>
2025-02-26 09:29:49 +0100

    yazi

--- modules/yazi/hm.nix
+++ modules/yazi/hm.nix
@@ -1,147 +1,186 @@
-# Based on the official catppuccin themes https://github.com/yazi-rs/themes
 {
   config,
   lib,
   ...
-}: {
+}:
+{
   options.stylix.targets.yazi = {
     enable = config.lib.stylix.mkEnableTarget "Yazi" true;
   };
 
   config = lib.mkIf (config.stylix.enable && config.stylix.targets.yazi.enable) {
-    programs.yazi.theme = with config.lib.stylix.colors.withHashtag; let
-      mkFg = fg: {inherit fg;};
-      mkBg = bg: {inherit bg;};
-      mkBoth = fg: bg: {inherit fg bg;};
-      mkSame = c: (mkBoth c c);
-    in {
-      manager = rec {
-        # Reusing bat themes, since it's suggested in the stying guide
-        # https://yazi-rs.github.io/docs/configuration/theme#manager
-        syntect_theme = config.lib.stylix.colors {
-          template = ../bat/base16-stylix.mustache;
-          extension = ".tmTheme";
+    programs.yazi.theme =
+      with config.lib.stylix.colors.withHashtag;
+      let
+        primary = base0D;
+        secondary = base0C;
+        tertiary = base08;
+        accent = base0E;
+        background = base00;
+        surface = base01;
+        surface1 = base02;
+        surface2 = base03;
+        # onSurface = base04;
+        onSurface1 = base05;
+        onSurface2 = base06;
+        red = base08;
+        green = base0B;
+        blue = base0D;
+        yellow = base0A;
+
+        mkFg = fg: { inherit fg; };
+        mkBg = bg: { inherit bg; };
+        mkBoth = fg: bg: { inherit fg bg; };
+        mkSame = c: (mkBoth c c);
+      in
+      {
+        manager = {
+          cwd = mkFg secondary;
+          hovered = {
+            reversed = true;
+          };
+          preview_hovered = {
+            underline = true;
+          };
+          find_keyword = {
+            fg = yellow;
+            bold = true;
+            italic = true;
+            underline = true;
+          };
+          find_position = {
+            fg = accent;
+            bg = "reset";
+            bold = true;
+            italic = true;
+          };
+          marker_copied = mkSame green;
+          marker_cut = mkSame red;
+          marker_marked = mkSame secondary;
+          marker_selected = mkSame yellow;
+          tab_active = {
+            reversed = true;
+          };
+          tab_inactive = { };
+          border_style = mkFg onSurface1;
         };
 
-        cwd = mkFg cyan;
-        hovered =
-          (mkBoth base05 base03)
-          // {
+        mode = {
+          normal_main = {
+            fg = background;
+            bg = primary;
             bold = true;
           };
-        preview_hovered = hovered;
-        find_keyword =
-          (mkFg green)
-          // {
+          normal_alt = mkBoth primary surface;
+          select_main = {
+            fg = background;
+            bg = secondary;
             bold = true;
           };
-        find_position = mkFg base05;
-        marker_selected = mkSame yellow;
-        marker_copied = mkSame green;
-        marker_cut = mkSame red;
-        tab_active = mkBoth base00 blue;
-        tab_inactive = mkBoth base05 base01;
-        border_style = mkFg base04;
-      };
-
-      mode = {
-        normal_main =
-          (mkBoth base00 blue)
-          // {
+          select_alt = mkBoth secondary surface;
+          unset_main = {
+            fg = background;
+            bg = tertiary;
             bold = true;
           };
-        normal_alt = mkBoth blue base00;
-        select_main =
-          (mkBoth base00 green)
-          // {
+          unset_alt = mkBoth tertiary surface;
+        };
+
+        status = {
+          progress_label = {
+            fg = "#ffffff";
             bold = true;
           };
-        select_alt = mkBoth green base00;
-        unset_main =
-          (mkBoth base00 brown)
-          // {
+          progress_normal = mkBoth primary surface1;
+          progress_error = mkBoth red surface1;
+          perm_sep = mkFg onSurface1;
+          perm_type = mkFg blue;
+          perm_read = mkFg yellow;
+          perm_write = mkFg red;
+          perm_exec = mkFg green;
+        };
+
+        pick = {
+          border = mkFg primary;
+          active = {
+            fg = accent;
             bold = true;
           };
-        unset_alt = mkBoth brown base00;
+          inactive = { };
+        };
+
+        input = {
+          border = mkFg primary;
+          selected = {
+            reversed = true;
+          };
+        };
+
+        completion = {
+          border = mkFg primary;
+        };
+
+        tasks = {
+          border = mkFg primary;
+          hovered = {
+            fg = accent;
+            underline = true;
+          };
+        };
+
+        which = {
+          mask = mkBg surface;
+          cand = mkFg secondary;
+          rest = mkFg onSurface2;
+          desc = mkFg accent;
+          separator_style = mkFg surface2;
+        };
+
+        help = {
+          on = mkFg secondary;
+          run = mkFg accent;
+          hovered = {
+            reversed = true;
+            bold = true;
+          };
+          footer = {
+            fg = surface;
+            bg = onSurface1;
+          };
+        };
+
+        notify = {
+          title_info = mkFg green;
+          title_warn = mkFg yellow;
+          title_error = mkFg red;
+        };
+
+        filetype.rules = [
+          {
+            mime = "image/*";
+            fg = secondary;
+          }
+          {
+            mime = "{audio,video}/*";
+            fg = yellow;
+          }
+          {
+            mime = "application/{zip,rar,7z*,tar,gzip,xz,zstd,bzip*,lzma,compress,archive,cpio,arj,xar,ms-cab*}";
+            fg = accent;
+          }
+          {
+            mime = "application/{pdf,doc,rtf}";
+            fg = green;
+          }
+          {
+            name = "*";
+            fg = onSurface1;
+          }
+          {
+            name = "*/";
+            fg = primary;
+          }
+        ];
       };
-
-      status = {
-        progress_label = mkBoth base05 base00;
-        progress_normal = mkBoth base05 base00;
-        progress_error = mkBoth red base00;
-        perm_type = mkFg blue;
-        perm_read = mkFg yellow;
-        perm_write = mkFg red;
-        perm_exec = mkFg green;
-        perm_sep = mkFg cyan;
-      };
-
-      pick = {
-        border = mkFg blue;
-        active = mkFg magenta;
-        inactive = mkFg base05;
-      };
-
-      input = {
-        border = mkFg blue;
-        title = mkFg base05;
-        value = mkFg base05;
-        selected = mkBg base03;
-      };
-
-      completion = {
-        border = mkFg blue;
-        active = mkBoth magenta base03;
-        inactive = mkFg base05;
-      };
-
-      tasks = {
-        border = mkFg blue;
-        title = mkFg base05;
-        hovered = mkBoth base05 base03;
-      };
-
-      which = {
-        mask = mkBg base02;
-        cand = mkFg cyan;
-        rest = mkFg brown;
-        desc = mkFg base05;
-        separator_style = mkFg base04;
-      };
-
-      help = {
-        on = mkFg magenta;
-        run = mkFg cyan;
-        desc = mkFg base05;
-        hovered = mkBoth base05 base03;
-        footer = mkFg base05;
-      };
-
-      # https://github.com/sxyazi/yazi/blob/main/yazi-config/preset/theme.toml
-      filetype.rules = let
-        mkRule = mime: fg: {inherit mime fg;};
-      in [
-        (mkRule "image/*" cyan)
-        (mkRule "video/*" yellow)
-        (mkRule "audio/*" yellow)
-
-        (mkRule "application/zip" magenta)
-        (mkRule "application/gzip" magenta)
-        (mkRule "application/tar" magenta)
-        (mkRule "application/bzip" magenta)
-        (mkRule "application/bzip2" magenta)
-        (mkRule "application/7z-compressed" magenta)
-        (mkRule "application/rar" magenta)
-        (mkRule "application/xz" magenta)
-
-        (mkRule "application/doc" green)
-        (mkRule "application/pdf" green)
-        (mkRule "application/rtf" green)
-        (mkRule "application/vnd.*" green)
-
-        ((mkRule "inode/directory" blue) // {bold = true;})
-        (mkRule "*" base05)
-      ];
-    };
   };
 }
